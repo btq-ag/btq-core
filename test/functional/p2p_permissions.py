df890481c682f7813fe0eeb570e671904cb5cfac
barneychambers <barneychambers@hotmail.com>
2026-09-08 11:22:34 +0100

    policy: default full replace-by-fee on
    New network, no opt-in-RBF culture to preserve. Keep -mempoolfullrbf
    so opt-in can still be forced. Tests that need signaling pin
    -mempoolfullrbf=0.

--- test/functional/p2p_permissions.py
+++ test/functional/p2p_permissions.py
@@ -9,7 +9,9 @@ Test that permissions are correctly calculated and applied
 
 from test_framework.messages import (
     SEQUENCE_FINAL,
+    CTxOut,
 )
+from test_framework.script import CScript, OP_TRUE
 from test_framework.p2p import P2PDataStore
 from test_framework.test_node import ErrorMatch
 from test_framework.test_framework import BTQTestFramework
@@ -111,15 +113,15 @@ class P2PPermissionsTests(BTQTestFramework):
             self.wait_until(lambda: txid in self.nodes[0].getrawmempool())
 
         self.log.debug("Check that node[1] will not send an invalid tx to node[0]")
-        tx.vout[0].nValue += 1
+        tx.vout.append(CTxOut(nValue=0, scriptPubKey=CScript([OP_TRUE])))
         txid = tx.rehash()
-        # Send the transaction twice. The first time, it'll be rejected by ATMP because it conflicts
-        # with a mempool transaction. The second time, it'll be in the m_recent_rejects filter.
+        # Dust is invalid independent of RBF signaling. The first send is
+        # rejected by ATMP; the second hits m_recent_rejects.
         p2p_rebroadcast_wallet.send_txs_and_test(
             [tx],
             self.nodes[1],
             success=False,
-            reject_reason='{} (wtxid={}) from peer=0 was not accepted: txn-mempool-conflict'.format(txid, tx.getwtxid())
+            reject_reason='{} (wtxid={}) from peer=0 was not accepted: dust'.format(txid, tx.getwtxid())
         )
 
         p2p_rebroadcast_wallet.send_txs_and_test(
